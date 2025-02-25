9a07416b6732c6bf0549628479561d3acf2d93c0
DmytroHalai <dimagalai04@gmail.com>
2025-02-25 19:13:06 +0200

    Fix code style

--- handlers.go
+++ handlers.go
@@ -8,11 +8,11 @@ import (
 )
 
 func TimeHandler(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
-  response := TimeResponse{Time: time.Now().Format(time.RFC3339)}
+	w.Header().Set("Content-Type", "application/json")
+	response := TimeResponse{Time: time.Now().Format(time.RFC3339)}
 
-  if err := json.NewEncoder(w).Encode(response); err != nil {
-    http.Error(w, "Failed to encode response", http.StatusInternalServerError)
-    log.Println("Error encoding JSON response:", err)
-  }
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+		log.Println("Error encoding JSON response:", err)
+	}
 }
