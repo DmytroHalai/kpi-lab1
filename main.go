f0c5090f2abdf3e8ca17feb134e98e045248937e
IYuriev <ilya.yuriev.s@gmail.com>
2025-02-25 18:59:11 +0200

    Add server connection logic

--- main.go
+++ main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+  mux := http.NewServeMux()
+  mux.HandleFunc("/time", TimeHandler)
+
+  server := &http.Server{
+    Addr:    ":8795",
+    Handler: mux,
+  }
+
+  server.ListenAndServe()
+  fmt.Println("Server is running at http://localhost:8795/time")
+}
