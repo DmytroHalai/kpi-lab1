003c422c3a868a997ebcd58a509dc661abcdbb5c
Solomka Andrii <andriissolomka@gmail.com>
2025-02-25 18:56:22 +0200

    Add error handler for TimeHandler

--- handlers.go
+++ handlers.go
@@ -1,13 +1,18 @@
 package main
 
 import (
-  "encoding/json"
-  "log"
-  "net/http"
-  "time"
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
 )
 
 func TimeHandler(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json")
   response := TimeResponse{Time: time.Now().Format(time.RFC3339)}
+
+  if err := json.NewEncoder(w).Encode(response); err != nil {
+    http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+    log.Println("Error encoding JSON response:", err)
+  }
 }
